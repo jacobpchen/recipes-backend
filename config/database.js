3c5c8da7661b8281913622e25017b909c2b615b6
Jacob Chen <jacobpchen@gmail.com>
2021-01-26 10:54:07 -0500

    changed config files to connect to MongoDB

--- config/database.js
+++ config/database.js
@@ -4,10 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        host: env('DATABASE_HOST', 'cluster0.ncavo.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'recipes'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'QTAIlxWjn9GC8Zn5'),
       },
       options: {
-        ssl: true,
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
